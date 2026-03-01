d09f6aeffc6e7746b1ed097226d76cafa93ec91e
hugocornellier <hugo.cornellier@gmail.com>
2026-03-01 15:44:01 -0400

    Add generated platform files from flutter build

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_litert_flex
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLitertFlexPlugin.register(with: registry.registrar(forPlugin: "FlutterLitertFlexPlugin"))
+}
---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_litert_flex/flutter_litert_flex_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterLitertFlexPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterLitertFlexPluginCApi"));
+}
